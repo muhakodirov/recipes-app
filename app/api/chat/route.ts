509013e7f465505f3c51b52241d3404b74a9db40
Muhammad <muhammadkodirov089@gmail.com>
2025-03-06 19:37:04 +0100

    fix

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,6 @@
 import { groq } from '@ai-sdk/groq';
 import { streamText } from 'ai';
-import { wrapLanguageModel, extractReasoningMiddleware } from 'ai';
 
-const enhancedModel = wrapLanguageModel({
-  model: groq('deepseek-r1-distill-llama-70b'),
-  middleware: extractReasoningMiddleware({ tagName: 'think' }),
-});
-// Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
@@ -15,7 +9,6 @@ export async function POST(req: Request) {
   const result = streamText({
     model: groq('gemma2-9b-it'),
     messages,
-    // enhancedModel(),
   });
 
   return result.toDataStreamResponse();
--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -5,15 +5,15 @@ import { useChat } from '@ai-sdk/react';
 import { ChefHatIcon, Send } from 'lucide-react';
 import ReactMarkdown from "react-markdown";
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit, isLoading, error, reload, stop } = useChat();
-  const {currUser} = useUserContext()
+  const { messages, input, handleInputChange, handleSubmit, isLoading, error, reload } = useChat();
+  const { currUser } = useUserContext()
 
- 
+
   return (
     <div className="flex flex-col overflow-x-auto">
       <div className='flex justify-center items-center space-x-2'>
-          <ChefHatIcon size={40} />
-          <span className='text-2xl'> RecipeAI </span>
+        <ChefHatIcon size={40} />
+        <span className='text-2xl'> RecipeAI </span>
       </div>
       <div className="flex-1 overflow-x-auto p-4 space-y-4 mt-5">
         {messages.map((m) => (
@@ -21,7 +21,7 @@ export default function Chat() {
             key={m.id}
             className={`p-2 rounded-md max-w-[80%] ${m.role === 'user' ? 'bg-blue-800 text-white self-end ml-auto' : 'bg-gray-700 text-white'}`}
           >
-            <span className="border rounded-md px-2 border-green-700 bg-slate-800">{m.role === 'user' ? `${currUser.firstname} ` : 'RecipeAI '} <br /> </span>
+            <span className="border rounded-md px-2 border-green-700 bg-slate-800">{m.role === 'user' ? `${currUser?.firstname} ` : 'RecipeAI '} <br /> </span>
             <ReactMarkdown>
               {m.content}
             </ReactMarkdown>
@@ -32,8 +32,8 @@ export default function Chat() {
       {error && (
         <div className="p-2 text-sm text-red-600 bg-red-100 border border-red-400 rounded-md shadow mt-2">
           {error.message}
-          <button 
-            className="ml-2 px-2 py-1 text-xs bg-red-500 text-white rounded-md" 
+          <button
+            className="ml-2 px-2 py-1 text-xs bg-red-500 text-white rounded-md"
             onClick={() => reload()}
           >
             Retry
